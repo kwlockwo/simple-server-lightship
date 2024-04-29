4a221fe54b4f4e11e2827d564de32f5772702269
Keith Lockwood <kwlockwo@gmail.com>
2024-04-29 15:19:39 +1000

    listen on 127.0.0.1

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const requestListener = function (req, res) {
     console.log(JSON.stringify(res.getHeaders()));
 }
 
-const server = http.createServer(requestListener).listen(port, '127.0.01', () => {
+const server = http.createServer(requestListener).listen(port, '127.0.0.1', () => {
     lightship.signalReady();
     console.log(`Node.js web server at port ${port} is running..`);
 })
